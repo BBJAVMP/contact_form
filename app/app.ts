dac07c053ef678d9afbaf599602b1bcfa60b97cf
BBJAVMP <julien1.feit@gmail.com>
2023-12-13 16:23:37 +0100

    attempt2

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,13 @@
-// Import required modules
+
 import express from 'express';
 import path from 'path';
 
-// Create an Express application
 const app = express();
 
-// Serve static files from the 'public' directory
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route for the root URL
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
